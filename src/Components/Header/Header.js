0b106894edcf8af5df5cc618c0ff2c2b16d4e2cd
sovsianykov <swebproger@gmail.com>
2021-01-23 03:32:02 +0200

    good start

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarBrand from 'react-bootstrap/NavbarBrand/package.json'
+function Header() {
+    return (
+        <>
+          <NavbarBrand bg = "info"  >
+
+          </NavbarBrand>
+
+        </>
+    );
+}
+
+export default Header;
